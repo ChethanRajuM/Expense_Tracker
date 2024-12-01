3c449b238412fd0029ccae93a50e91ca977f047d
Muppala Chethan Raju <muppalachethan123@gmail.com>
2024-12-01 20:08:38 +0530

    'updated'

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_TRACKER_DIRS = os.path.join(BASE_DIR,'tracker','templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = "expensetracker.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_TRACKER_DIRS],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
